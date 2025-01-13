618f1df1dff577548df8050ec32e5e70786683cb
Corguts <corentin.quesnel@outlook.fr>
2025-01-14 00:09:27 +0100

    end

--- js/javascript_eval.js
+++ js/javascript_eval.js
@@ -1,7 +1,7 @@
 // Initialisation de la carte Leaflet sans vue initiale
 var map = L.map('map');
-var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib = 'Map data © OpenStreetMap contributors';
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';  // URL de l'API OSM
+var osmAttrib = 'Map data © OpenStreetMap contributors';  // Attribution OSM
 var osm = new L.TileLayer(osmUrl, { attribution: osmAttrib }).addTo(map);
 
 // Ajouter l'échelle
@@ -9,9 +9,9 @@ L.control.scale().addTo(map);
 
 // Gestionnaire de couches
 var baseLayers = {
-    "OpenStreetMap": osm
+    "OpenStreetMap": osm  // Couche OpenStreetMap
 };
-var overlays = {};
+var overlays = {}; // Tableau pour stocker les couches
 
 // Initialisation du gestionnaire de couches
 var layerControl = L.control.layers(baseLayers, overlays).addTo(map);
@@ -27,7 +27,7 @@ var customIcon = L.icon({
     popupAnchor: [0, -16] // Point d'ancrage du popup
 });
 
-// Tableau de couleurs prédéfinies
+// Tableau de couleurs prédéfinies pour les lignes
 const colors = [
     'rgb(255, 238, 0)',    // jaune
     'rgb(255, 0, 0)',      // rouge
@@ -42,15 +42,15 @@ let colorIndex = 0; // Index pour parcourir le tableau de couleurs
 
 
 // Fonction pour charger et ajouter un fichier JSON à la carte
-function loadJSON(filePath, layerName) {
-    fetch(filePath)
-        .then(response => response.json())
-        .then(data => {
-            var geojsonLayer = L.geoJSON(data, {
-                pointToLayer: function (feature, latlng) {
-                    return L.marker(latlng, { icon: customIcon });
+function loadJSON(filePath, layerName) { 
+    fetch(filePath) // Utilisation de fetch pour charger le fichier JSON
+        .then(response => response.json()) // Conversion de la réponse en JSON
+        .then(data => {  // Traitement des données JSON
+            var geojsonLayer = L.geoJSON(data, {  // Création de la couche GeoJSON
+                pointToLayer: function (feature, latlng) {  // pointToLayer pour personnaliser l'icône pour les points
+                    return L.marker(latlng, { icon: customIcon });  
                 },
-                style: function (feature) {
+                style: function (feature) {  // Style pour les géométries
                     if (feature.geometry.type === 'MultiPolygon') {
                         return { className: 'jardin' }; // Classe CSS pour les jardins
                     } else if (feature.geometry.type === 'Polygon') {
@@ -58,12 +58,12 @@ function loadJSON(filePath, layerName) {
                     } else if (feature.geometry.type === 'MultiLineString') {
                         const rgbColor = colors[colorIndex % colors.length];
                         colorIndex++; // Incrémenter l'index pour la prochaine couleur
-                        return { color: rgbColor, weight: 5 };
+                        return { color: rgbColor, weight: 5 }; // Style pour les lignes
                     } else {
                         return { color: 'blue', weight: 2 }; // Style par défaut
                     }
                 },
-                onEachFeature: function (feature, layer) {
+                onEachFeature: function (feature, layer) { // Ajouter un événement de survol pour afficher le cercle pour les géométries de type Point avec un rayon de 300m et un style
                     if (feature.geometry.type === 'Point') {
                         var latlng = L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0]);
                         var circle = L.circle(latlng, {
@@ -71,11 +71,11 @@ function loadJSON(filePath, layerName) {
                             color: 'red',
                             fillOpacity: 0.1
                         });
-
+                        // le mouseover ajoute le cercle
                         layer.on('mouseover', function () {
                             circle.addTo(map);
                         });
-
+                        // le mouseout supprime le cercle 
                         layer.on('mouseout', function () {
                             map.removeLayer(circle);
                         });
@@ -85,12 +85,12 @@ function loadJSON(filePath, layerName) {
                     
                     // Ajouter un événement de clic pour afficher les informations
 layer.on('click', function () {
-    var infoContent = document.getElementById('info-content');
+    var infoContent = document.getElementById('info-content');  
 
     // Vérifier le type de géométrie et afficher les informations en conséquence
-    if (feature.geometry.type === 'Polygon' || feature.geometry.type === 'MultiPolygon') {
+    if (feature.geometry.type === 'Polygon' || feature.geometry.type === 'MultiPolygon') { //si c'est un polygone ou un ensemble de polygones
         // Informations pour les polygones (parcs/jardins par exemple)
-        infoContent.innerHTML = `
+        infoContent.innerHTML = ` 
             <table>
                 <tr><th>Nom</th><td>${feature.properties.nom || 'N/A'}</td></tr>
                 <tr><th>Surface totale (m²)</th><td>${feature.properties.surf_tot_m2 || 'N/A'}</td></tr>
@@ -103,7 +103,7 @@ layer.on('click', function () {
                 <tr><th>Chien</th><td>${feature.properties.chien || 'N/A'}</td></tr>
                 <tr><th>Espace canin</th><td>${feature.properties.esp_can || 'N/A'}</td></tr>
             </table>
-        `;
+        `;  // Afficher les informations dans le tableau HTML
     } else if (feature.geometry.type === 'LineString' || feature.geometry.type === 'MultiLineString') {
         // Informations pour les lignes (par exemple, lignes de métro)
         infoContent.innerHTML = `
@@ -151,19 +151,20 @@ layer.on('click', function () {
             markers.addLayer(geojsonLayer);
             map.addLayer(markers);
 
+            // Ajouter la couche au gestionnaire de couches
             overlays[layerName] = markers;
-            layerControl.addOverlay(markers, layerName);
+            layerControl.addOverlay(markers, layerName);  //overlay sert pour afficher la legende
 
             // Ajouter les limites de la couche au tableau
             allLayersBounds.push(geojsonLayer.getBounds());
 
             // Vérifier si toutes les couches sont chargées
-            if (allLayersBounds.length === 3) {
-                var combinedBounds = L.latLngBounds(allLayersBounds);
+            if (allLayersBounds.length === 3) { // 3 couches chargées
+                var combinedBounds = L.latLngBounds(allLayersBounds);  // Combinaison des limites pour le zoom de début
                 map.fitBounds(combinedBounds);
             }
         })
-        .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));
+        .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));  // Gestion des erreurs dans la console si problème 
 }
 
 // Charger les fichiers JSON et les ajouter au gestionnaire de couches
@@ -181,30 +182,30 @@ function updateLegend() {
         const layer = overlays[layerName];
 
         if (map.hasLayer(layer)) {
-            if (layerName === 'Lyon Parcs et Jardins') {
+            if (layerName === 'Lyon Parcs et Jardins') {  // Ajout de la condition pour Lyon Parcs et Jardins
                 legendContainer.innerHTML += `
                     <div>
                         <span class="legend-shape jardin"></span> Parcs et jardins
                     </div>`;
-            } else if (layerName === 'Sytral TCL') {
+            } else if (layerName === 'Sytral TCL') { // Ajout de la condition pour le Sytral TCL
                 legendContainer.innerHTML += `
                     <div>
                         <div class="legend-line metro-line"></div> Lignes de métro
                     </div>`;
-            } else if (layerName === 'TCL Stations') {
+            } else if (layerName === 'TCL Stations') {  // Ajout de la condition pour TCL Stations
                 legendContainer.innerHTML += `
                     <div>
-                        <img src="http://www.clipartbest.com/cliparts/niB/Edk/niBEdkxXT.png" width="20" height="20"> Entrées/sorties stations
-                    </div>`;
+                        <img src="http://www.clipartbest.com/cliparts/niB/Edk/niBEdkxXT.png" width="20" height="20"> Entrées/sorties stations  
+                    </div>`;// Ajout de l'URL de l'image pour l'icône
             }
         }
     }
 }
 
 // Ajouter les écouteurs d'événements pour la mise à jour de la légende
-map.on('overlayadd', updateLegend);
-map.on('overlayremove', updateLegend);
-map.on('moveend', updateLegend);
+map.on('overlayadd', updateLegend);   //overlayadd sert lors de l'ajout
+map.on('overlayremove', updateLegend);  //overlayremove sert lors de la suppression
+
 
 // Appeler la fonction au chargement initial
 updateLegend();
